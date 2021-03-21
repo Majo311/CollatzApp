4bcdb9cb20e85f3868b8afef5d4f474d5b480e0e
Mario S <mario.smalo@gmail.com>
2021-03-21 13:17:03 +0100

    add TypeHelper and method GetDigits,create class class CollatzCalculator

--- CollatzApp/CollatzCalculator/CollatzCalculator.cs
+++ CollatzApp/CollatzCalculator/CollatzCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CollatzCalculator
 {
-    public class Class1
+    public class CollatzCalculator
     {
     }
 }
---
+++ CollatzApp/CollatzCalculator/TypeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollatzCalculator
+{
+    public static class TypeHelper
+    {
+        public static IEnumerable<uint> GetDigits(this string txtValue)
+        {
+            uint result;
+            foreach (char digit in txtValue)
+            {
+                if (!uint.TryParse(digit.ToString(), out result))
+                {
+                    throw new Exception("Incorrect parameter txtValue, the Input is incorrect!");
+                }
+                yield return result;
+            }
+        }
+    }
+}
