24f67205d6628cc7764dd3012cb933e6248dcd89
Mario S <mario.smalo@gmail.com>
2021-03-21 13:19:34 +0100

    Add method Calculate to calculate results

--- CollatzApp/CollatzCalculator/CollatzCalculator.cs
+++ CollatzApp/CollatzCalculator/CollatzCalculator.cs
@@ -8,5 +8,49 @@ namespace CollatzCalculator
 {
     public class CollatzCalculator
     {
+        public List<string> SequenceValues
+        {
+            get;
+            private set;
+        } = new List<string>();
+        public uint NumberOfEven
+        {
+            get;
+            private set;
+        } = 0;
+        public uint NumberOfOdd
+        {
+            get;
+            private set;
+        } = 0;
+        public List<uint> InputValue { get; private set; }
+        public CollatzCalculator(string txtInput)
+        {
+            this.InputValue = txtInput.GetDigits().ToList();
+        }
+        public void Calculate()
+        {
+            double result = 0;
+            for (int i = 0; i < this.InputValue.Count; i++)
+            {
+                result += this.InputValue[i] * Math.Pow(10, this.InputValue.Count - 1 - i);
+            }
+            double sequenceItem = result;
+            do
+            {
+                if (sequenceItem % 2 == 0)
+                {
+                    sequenceItem = sequenceItem / 2;
+                    this.NumberOfEven++;
+                }
+                else
+                {
+                    sequenceItem = (3 * sequenceItem) + 1;
+                    this.NumberOfOdd++;
+                }
+                this.SequenceValues.Add(sequenceItem.ToString());
+            }
+            while (sequenceItem != 1);
+        }
     }
 }
