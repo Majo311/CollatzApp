f2dede46074de1064834ee7661f54e9f3565b1fe
Mario S <mario.smalo@gmail.com>
2021-03-21 12:53:08 +0100

    add CollatzCalculator dll

---
+++ CollatzApp/CollatzCalculator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzCalculator
+{
+    public class Class1
+    {
+    }
+}
